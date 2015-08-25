7fea6f5b2dd98658062fd4a7b8969be5389c4b59
Your Name <you@example.com>
2015-08-25 21:35:12 +0600

    init

---
+++ trunk/zkcdi/zkcdi/src/org/zkoss/cdi/annotation/impl/EventBinding.java
@@ -0,0 +1,29 @@
+/* EventBinding.java
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Jan 25, 2010 17:43:54 PM, Created by ashish
+}}IS_NOTE
+
+Copyright (C) 2010 Potix Corporation. All Rights Reserved.
+
+*/
+package org.zkoss.cdi.annotation.impl;
+
+import javax.enterprise.util.AnnotationLiteral;
+
+import org.zkoss.cdi.event.Events;
+
+/**
+ * <p> An abstract class used while publishing ZK events using 
+ * CDI event notification mechanism
+ * </p>
+ * @author ashish
+ */
+@SuppressWarnings("serial")
+public abstract class EventBinding extends AnnotationLiteral<Events> 
+	implements Events {
+}
---
+++ trunk/zkcdi/zkcdi/src/org/zkoss/cdi/weld/Version.java
@@ -0,0 +1,22 @@
+/* Version.java
+
+	Purpose:
+		
+	Description:
+		
+	History:
+		Dec 30, 2009 5:18:13 PM, Created by henrichen
+
+Copyright (C) 2009 Potix Corporation. All Rights Reserved.
+
+*/
+package org.zkoss.cdi.weld;
+
+/**
+ * The version of the ZK CDI Extension & Utilities.
+ * @author henrichen
+ */
+public class Version {
+	/** Returns the version UID */
+	public static final String UID = "1.0RC";
+}
